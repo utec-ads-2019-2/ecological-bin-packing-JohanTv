798ea7fe44b947b50766c0cbe89eb3f132a830a4
JohanTv <40219652+JohanTv@users.noreply.github.com>
2019-08-26 02:54:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
-
+#include <map>
+#include <string>
+#include <algorithm>
 using namespace std;
+typedef pair<int, string> combinacion;
+
+int brown[3];
+int green[3];
+int clear[3];
+map<int, string> combinaciones;
+bool leerDatos(){
+    if(scanf("%u %u %u %u %u %u %u %u %u ",
+            &brown[0],&green[0],&clear[0],
+             &brown[1],&green[1],&clear[1],
+             &brown[2],&green[2],&clear[2])==9)
+        return true;
+    return false;
+}
+
+
+void minMovimientos(){
+    combinaciones.insert(combinacion(brown[1]+brown[2]+ green[0] + green[1] + clear[0] + clear[2],"BCG"));
+    combinaciones.insert(combinacion(brown[1]+brown[2]+ green[0] + green[2] + clear[0] + clear[1],"BGC"));
+    combinaciones.insert(combinacion(brown[0]+brown[2]+ green[0] + green[1] + clear[1] + clear[2],"CBG"));
+    combinaciones.insert(combinacion(brown[0]+brown[1]+ green[0] + green[2] + clear[1] + clear[2],"CGB"));
+    combinaciones.insert(combinacion(brown[0]+brown[2]+ green[1] + green[2] + clear[0] + clear[1],"GBC"));
+    combinaciones.insert(combinacion(brown[0]+brown[1]+ green[1] + green[2] + clear[0] + clear[2],"GCB"));
+
+    auto it = combinaciones.begin();
+    cout<<it->second<<" "<<it->first<<endl;
+    combinaciones.clear();
+}
 
 int main() {
-    return EXIT_SUCCESS;
+
+    while(leerDatos()){
+        minMovimientos();
+    }
+
 }
